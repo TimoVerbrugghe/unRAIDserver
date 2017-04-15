32a26266ace6a11e35c163c6f9335f1ed34ee651
Timo Verbrugghe <timo@hotmail.be>
2017-04-15 16:16:12 +0200

    Update Security.sh

--- Services/Security.sh
+++ Services/Security.sh
@@ -43,6 +43,7 @@
 			# SSH jail (in a seperated /etc/fail2ban/jail.d/ssh-iptables.conf)
 				nano /etc/fail2ban/jail.d/ssh-iptables.conf
 					IgnoreIP = 127.0.0.0/8 10.124.161.0/24
+					# Delete sendmail-whois action
 		
 		systemctl enable fail2ban
 		systemctl start fail2ban
