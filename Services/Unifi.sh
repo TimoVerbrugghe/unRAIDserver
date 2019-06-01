81ab9f43c75387005bb2f81672670ba0af5dbb97
Timo Verbrugghe <timo@hotmail.be>
2019-06-01 22:31:36 +0200

    Update Unifi.sh
    Changed mongodb package + added additional instructions

--- Services/Unifi.sh
+++ Services/Unifi.sh
@@ -1,17 +1,29 @@
- ## Unifi Controller installation on ArchServer
+ ## Unifi Controller installation on ArchServer 
+ # Add firewall exceptions
+ 	# Add 8080/tcp, 8443/tcp & 3487/udp to the UFW firewall
+ 	ufw allow from 192.168.0.0/24 to any port 8080 proto tcp
+ 	ufw allow from 192.168.0.0/24 to any port 8443 proto tcp
+ 	ufw allow from 192.168.0.0/24 to any port 3487 proto udp
+
+# Install correct mongodb package
+	yay -Syu mongodb-bin # don't install the regular mongodb package, since it takes several hours to compile. monogodb-bin is a precompiled package
+
  # Install unifi package
- 	pacman -Syu unifi
+ 	yay -Syu unifi
 
  # Enable & start unifi controller
  	systemctl enable unifi
  	systemctl start unifi
 
- # Go to localhost:8443 to start configuration
+ # SSH into the Unifi AP access point and perform the following commands
+ 	set-default #resets device
+ 	set-inform http://<ip-address of server>:8080/inform # makes sure unifi ap is announcing itself to the correct controller
+
+# Go to https://<ip-address of server>:8443 to start configuration
+
  	# Don't forget to do an RF environment scan to set up the best channels 
  	# Set up static IP
  	# Set up controller name, cloud access & Site name
-
- # Add firewall exceptions
- 	# Port 8080 & 8443 on local network
+ 	# Set up cloud access and that you can sign in with your UBNT account at https://unifi.ubnt.com
 
  # Run systemd service certbot to enable SSL on unifi controller
