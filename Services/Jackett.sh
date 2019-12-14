3474a97288a2ff99ec1d0594c603534376aab22b
Timo Verbrugghe <timo@hotmail.be>
2019-12-14 13:53:43 +0100

    Deletion of lidarr & changes to jackett

--- Services/Jackett.sh
+++ Services/Jackett.sh
@@ -2,8 +2,8 @@
 # Install the latest realease from Jackett in ~/Applications/jackett (from https://github.com/Jackett/Jackett)
 
 # Move systemd service to /etc/systemd/system/jackett.service
-	systemctl enable jackett-public.service
-	systemctl start jackett-public.service
+	systemctl enable jackett.service
+	systemctl start jackett.service
 
 # Go to port 9117 on Jackket
 # Add all public trackers (see Github for latest list) & change port to 8088, "Base Path Override" to /jackett for apache proxy forwarding
