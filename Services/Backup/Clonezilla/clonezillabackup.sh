dea4ec9ce54d1d8b4a18d15ae4123f2c0b8ac57d
Timo Verbrugghe <fileserver@ArchServer.localdomain>
2020-01-18 01:24:14 +0100

    Preparing to reenable clonezillabackup

--- Services/Backup/Clonezilla/clonezillabackup.sh
+++ Services/Backup/Clonezilla/clonezillabackup.sh
@@ -7,35 +7,28 @@
 source /home/fileserver/Applications/ArchServer/config/variables.sh
 
 # Remove old backup logs, recreate them & set right permissions
-rm -rf /home/fileserver/Applications/Backup/logs/backup.log >/dev/null 2>&1
-rm -rf /home/fileserver/Applications/Backup/logs/rsync.log >/dev/null 2>&1
-rm -rf /home/fileserver/Applications/Backup/logs/windowsvmuserfolder.log >/dev/null 2>&1
-rm -rf /home/fileserver/Applications/btrfscheck/btrfscheck.log >/dev/null 2>&1
-rm -rf /home/fileserver/Applications/autoupdate/autoupdate.log >/dev/null 2>&1
-rm -rf /home/fileserver/Applications/ARM/logs/* >/dev/null 2>&1
+rm -rf $BACKUP_LOG >/dev/null 2>&1
+rm -rf $RCLONE_LOG >/dev/null 2>&1
 
-touch /home/fileserver/Applications/Backup/logs/backup.log >/dev/null 2>&1
-touch /home/fileserver/Applications/Backup/logs/rsync.log >/dev/null 2>&1
-touch /home/fileserver/Applications/Backup/logs/windowsvmuserfolder.log >/dev/null 2>&1
-touch /home/fileserver/Applications/btrfscheck/btrfscheck.log >/dev/null 2>&1
-touch /home/fileserver/Applications/autoupdate/autoupdate.log >/dev/null 2>&1
+touch $BACKUP_LOG >/dev/null 2>&1
+touch $RCLONE_LOG >/dev/null 2>&1
 
-chmod 755 -R /home/fileserver/Applications/Backup/logs >/dev/null 2>&1
-chown fileserver:fileserver -R /home/fileserver/Applications/Backup/logs >/dev/null 2>&1
+chmod 755 -R $BACKUP_LOG >/dev/null 2>&1
+chown fileserver:fileserver -R $BACKUP_LOG >/dev/null 2>&1
 
-chmod 755 -R /home/fileserver/Applications/btrfscheck/btrfscheck.log >/dev/null 2>&1
-chown fileserver:fileserver -R /home/fileserver/Applications/btrfscheck/btrfscheck.log >/dev/null 2>&1
+chmod 755 -R $RCLONE_LOG >/dev/null 2>&1
+chown fileserver:fileserver -R $RCLONE_LOG >/dev/null 2>&1
 
 # Starting Monthly/Clonezilla Backup
-printf "Starting Monthly/Clonezilla Backup. Backup logs have been reset. Time & Date right now is $(date)\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Starting Monthly/Clonezilla Backup. Backup logs have been reset. Time & Date right now is $(date)\n" >> $BACKUP_LOG 2>&1
 
 # Remove old backup
-printf "Preparing Clonezilla Backup\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
-printf "Removing old backup\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
-rm -rf /home/fileserver/Media/SystemImage/Fileserver >/dev/null 2>&1
+printf "Preparing Clonezilla Backup\n" >> $BACKUP_LOG 2>&1
+printf "Removing old backup\n" >> $BACKUP_LOG 2>&1
+rm -rf $MEDIA_LOCATION/SystemImage/Fileserver >/dev/null 2>&1
 
 # Clean up caches
-printf "Cleaning caches\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Cleaning caches\n" >> $BACKUP_LOG 2>&1
 yes | paccache -rk 0 >/dev/null 2>&1
 yes | paccache -ruk0 >/dev/null 2>&1
 yes | yay -Scc --noconfirm 2>&1 || true && # It can be that command fails because no packages to delete, in that case, still give out value true so script can continue
@@ -53,20 +46,19 @@ rm -rf /home/fileserver/.bzr.log 2>&1 &&
 rm -rf /home/fileserver/.wget-hsts 2>&1 &&
 rm -rf /home/fileserver/.thumbnails 2>&1 &&
 
-printf "Cleaning Desktop & DriveRip folder\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Cleaning Desktop folder\n" >> $BACKUP_LOG 2>&1
 rm -rf /home/fileserver/Desktop/* 2>&1 &&
-rm -rf /home/fileserver/Media/DriveRip/* 2>&1 &&
 
-printf "Cleaning completed Downloads folder\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Cleaning completed Downloads folder\n" >> $BACKUP_LOG 2>&1
 rm -rf /home/fileserver/Media/Downloads/complete/tvshows/* 2>&1
 rm -rf /home/fileserver/Media/Downloads/complete/movies/* 2>&1
 
 # Removing unnecessary packages
-printf "Removing unnecessary packages\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Removing unnecessary packages\n" >> $BACKUP_LOG 2>&1
 yes | pacman -Rns $(pacman -Qtdq) 2>&1 || true && # It can be that command fails because no packages to delete, in that case, still give out value true so script can continue
 
 # Setup of boot environment
-printf "Setting up boot environment\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Setting up boot environment\n" >> $BACKUP_LOG 2>&1
 
 # Set default boot entry
 grub-set-default 0 >/dev/null 2>&1
@@ -75,9 +67,9 @@ grub-set-default 0 >/dev/null 2>&1
 grub-reboot "Clonezilla (Arch Backup)" >/dev/null 2>&1
 
 # Send a notification to system administrator that a backup is about to happen
-printf "Send notification to System Administrator\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Send notification to System Administrator\n" >> $BACKUP_LOG 2>&1
 $PUSHBULLET_SCRIPT "ArchServer: Monthly Backup Started" "Backup of Internal SSD to an image using Clonezilla has begun. All configuration files are in ~/Applications/Backup. Manual Clonezilla backup also possible using interactive Clonezilla environment (sudo grub-reboot \"Clonezilla (interactive)\")" >/dev/null 2>&1 &&
 
 # Reboot the system to initiate the backup
-printf "Rebooting system to start Clonezilla Backup\n\n" >>/home/fileserver/Applications/Backup/logs/backup.log 2>&1
+printf "Rebooting system to start Clonezilla Backup\n\n" >> $BACKUP_LOG 2>&1
 reboot
--- config/variables.sh
+++ config/variables.sh
@@ -1,14 +1,14 @@
 PUSHBULLET_SCRIPT="/home/fileserver/Applications/ArchServer/Services/pushbullet.sh"
-RCLONE_LOG="/home/fileserver/Applications/ArchServer/Services/Backup/rclone.log"
 RCLONE_EXCLUSIONS="/home/fileserver/Applications/ArchServer/config/rclone_exclusions"
-BACKUP_LOG="/home/fileserver/Applications/ArchServer/Services/Backup/backup.log"
+BACKUP_LOG="/home/fileserver/Applications/logs/backup.log"
+RCLONE_LOG="/home/fileserver/Applications/logs/rclone.log"
+RESTORE_LOG="/home/fileserver/restore.log"
+
 MEDIA_LOCATION="/home/fileserver/Media"
 BACKUP_LOCATION="/home/fileserver/Backup"
 BACKUP_FOLDERS=(Applications Books Games Movies Music Network OSInstallISO Photos Software SystemImage TVShows)
 ERRORVALUE=0
-RESTORE_LOG="/home/fileserver/restore.log"
 
-BACKUP_LOCATION="/home/fileserver/Media" # The location where there are currently files that you want to restore to another location
 RESTORE_LOCATION="/home/fileserver/Temporary" # The location where you want to restore files to
 
-RESTORE_FOLDERS=(Applications Books Games Movies Music Network OldServer OSInstallISO Photos Software SystemImage TVShows)
+RESTORE_FOLDERS=(Applications Games Movies Network Photos SystemImage TVShows)
